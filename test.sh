02d6d12db9357cdc0c5486c8120904b6d9bb7f49
huanghaian <huanghaian@sensetime.com>
2024-09-11 10:53:31 +0800

    update

--- test.sh
+++ test.sh
@@ -11,3 +11,5 @@ echo 'test/test_hybrid_attn.py'
 srun -p llm_razor -N 1 --gres=gpu:4 --ntasks=1 --cpus-per-task=16 --time=1:00:00 torchrun --nproc-per-node=4 test/test_hybrid_attn.py
 echo 'test/test_hybrid_qkvpacked_attn.py'
 srun -p llm_razor -N 1 --gres=gpu:4 --ntasks=1 --cpus-per-task=16 --time=1:00:00 torchrun --nproc-per-node=4 test/test_hybrid_qkvpacked_attn.py
+echo 'test/test_llama3_flash_attn_varlen_func.py'
+srun -p llm_razor -N 1 --gres=gpu:4 --ntasks=1 --cpus-per-task=16 --time=1:00:00 torchrun --nproc-per-node=4 test/test_llama3_flash_attn_varlen_func.py
